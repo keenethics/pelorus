5f4912722088d7c2049ba5dcb589d3a2d2b0cd17
Epaminond <k4merton@gmail.com>
2015-10-11 00:00:55 +0300

    Drop Milestones.getPeriod (ref #7)

--- models/Milestones.js
+++ models/Milestones.js
@@ -8,13 +8,6 @@ Milestones.attachSchema(new SimpleSchema({
   parentMilestoneId: { type: String, label: "Parent Milestone ID", optional: true }
 }));
 
-Milestones.getPeriod = function(startsAt, type = 'year') {
-  return {
-    startsAt: Milestones.roundedBound(startsAt, 'start'),
-    endsAt:   Milestones.roundedBound(startsAt.endOf(type), 'end')
-  }
-}
-
 Milestones.roundedBound = (date, type = 'start') => {
   if(type == 'start' && date.day() > 4)  date = date.add(1, 'w');
   if(type == 'end'   && date.day() <= 4) date = date.subtract(1, 'w');
