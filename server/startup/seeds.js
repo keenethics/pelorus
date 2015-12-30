7d090fbb31ee194a96f53f5263d8f55093e00264
yataras <taras.yaskiv@gmail.com>
2015-12-30 13:07:54 +0200

    add seeds of milestones and goals

--- server/startup/seeds.js
+++ server/startup/seeds.js
@@ -16,6 +16,46 @@ if ( Meteor.isServer && mode === 'development' ) {
           'last_name': 'lname',
         },
       });
+      Milestones.insert({
+        'period': 2016,
+        'type': 'year',
+        'userId': Meteor.users.findOne({'username': 'test'})._id,
+        'parentId': '',
+        'startsAt': '',
+        'endsAt': '',
+      });
+      Milestones.insert({
+        'period': '2016-1',
+        'type': 'month',
+        'userId': Meteor.users.findOne({'username': 'test'})._id,
+        'parentId': Milestones.findOne({'type': 'year'})._id,
+        'startsAt': '',
+        'endsAt': '',
+      });
+      Milestones.insert({
+        'period': '2016-W3',
+        'type': 'week',
+        'userId': Meteor.users.findOne({'username': 'test'})._id,
+        'parentId': Milestones.findOne({'type': 'month'})._id,
+        'startsAt': '',
+        'endsAt': '',
+      });
+      Goals.insert({
+        'title': 'Made list of tasks',
+        'parentId': '',
+        'milestoneId': Milestones.findOne({'type': 'year'})._id,
+        'userId': Meteor.users.findOne({'username': 'test'})._id,
+        'completedPct': '',
+        'pctOfParentGoal': '',
+      });
+      Goals.insert({
+        'title': 'Made list task #1',
+        'parentId': Goals.findOne({'title': 'Made list of tasks'})._id,
+        'milestoneId': Milestones.findOne({'type': 'month'})._id,
+        'userId': Meteor.users.findOne({'username': 'test'})._id,
+        'completedPct': '',
+        'pctOfParentGoal': '',
+      });
     }
   });
 }
