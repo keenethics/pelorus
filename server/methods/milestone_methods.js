a50735b2e48198c295328bd2cf28ffa20f9cacf2
OksentukVlad <oksentukvlad@gmail.com>
2015-12-30 18:28:41 +0200

    Replaced tabs with double-spaces.

--- server/methods/milestone_methods.js
+++ server/methods/milestone_methods.js
@@ -1,14 +1,12 @@
 Meteor.methods({
-	'insertGoal': function (data,a,b) {
-		if (!data.userId) return
-		check(data,Object)
-		// we currently don`t have milestoneId field in new goal object to create
-		_.extend(data,{milestoneId: 'eqw213eqwe1213edas'})
+  'insertGoal': function (data, a, b) {
+    if (!data.userId) return;
+    check(data, Object);
+    // we currently don`t have milestoneId field in new goal object to create
+    _.extend(data, {milestoneId: 'eqw213eqwe1213edas'});
 
-		Goals.insert(data, function (err) {
-			if (err) console.log("error"+ err);
-		});
-	}
-
-
-})
+    Goals.insert(data, function(err) {
+      if (err) console.log('error' + err);
+    });
+  }
+});
