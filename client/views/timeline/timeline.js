75a3212887dd81d3023d525c19f944d15aa35f8f
Nazar Yablonskiy <nazaryablonskiy@gmail.com>
2015-12-30 12:53:48 +0200

    fixed timeline.js

--- client/views/timeline/timeline.js
+++ client/views/timeline/timeline.js
@@ -1,78 +1,59 @@
-Template.timeline.onCreated(() => {
+Template.timeline.onCreated(function(){
   this.currentView = new ReactiveVar('week');
 });
 
 Template.timeline.helpers({
-  strategicMilestone: () => {
+  strategicMilestone: function () {
     return Milestones.findOne({userId: Meteor.userId(), type: 'strategic'});
   },
-  currentView: () =>{
+  currentView: function(){
     return Template.instance().currentView.get();
-  },
+  }
 });
 
 Template.timeline.events({
-  'click .view-type': (e, t) => {
-    let newType = e.currentTarget.dataset.viewType;
+  'click .view-type': function (e, t) {
+    var newType = e.currentTarget.dataset.viewType;
     newType && t.currentView.set(newType);
-  },
+  }
 });
 
 Template.milestoneAlt.events({
-  'click .js-add-goal': () => {
-    let data = { milestone: this.milestone };
-    Blaze.renderWithData(Template._formModal, {
-      title: 'Add Goal',
-      template: '_goalsForm',
-      data,
-    }, document.body);
-  },
-  'click .js-add-milestone': () => {
-    let data = { type: this.milestone.childType };
-    Blaze.renderWithData(Template._formModal, {
-      title: 'Add Milestone',
-      template: '_milestonesForm',
-      data,
-    }, document.body);
+  'click .js-add-goal': function(e) {
+    var data = { milestone: this.milestone }
+    Blaze.renderWithData(Template._formModal, { title: 'Add Goal', template: '_goalsForm', data }, document.body);
   },
 });
 
 Template.milestoneAlt.helpers({
-  'getTitle': () => {
-    let milestone = this.milestone;
-    if (milestone) {
-      switch (milestone.type) {
-      case 'strategic':
-        return 'Strategic';
-      case 'year':
-        return milestone.period;
-      case 'month':
-        let monthNames = ['January', 'February',
-          'March', 'April', 'May', 'June',
-          'July', 'August', 'September',
-          'October', 'November', 'December',
-        ];
-        let date = new Date(milestone.period);
-        return monthNames[date.getMonth()];
-      case 'week':
-        return 'Week#' + moment(milestone.period, 'yyyy-ww').week();
-      default:
-        return '';
+  'getTitle': function() {
+    var milestone = this.milestone;
+    if (milestone){
+      switch (milestone.type){
+        case 'strategic':
+          return 'Strategic';
+        case 'year':
+          return milestone.period
+        case 'month':
+          var monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+              ];
+          var date = new Date(milestone.period);
+          return monthNames[date.getMonth()]
+        case 'week':
+          return 'Week#' + moment(milestone.period, 'yyyy-ww').week();
       }
     }
   },
-  'childType': () => {
-    if (this.milestone) {
-      switch (this.milestone.type) {
+  'childType': function(){
+    var type = this.milestone.type;
+    switch (this.milestone.type){
       case 'strategic':
         return 'year';
       case 'year':
         return 'month';
       case 'month':
         return 'week';
-      default:
-        return '';
-      }
     }
-  },
+  }
 });
