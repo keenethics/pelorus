e5a30837352fc34541f034e635684a8365826cd8
Epaminond <k4merton@gmail.com>
2016-01-01 22:32:08 +0200

    Fix opening multiple goal forms

--- client/views/milestones/_level.js
+++ client/views/milestones/_level.js
@@ -1,23 +1,5 @@
 Template._milestoneLevel.events({
-  'click .js-add-goal': function() {
-    let data = { 'milestone': this.milestone };
-    Blaze.renderWithData(Template._formModal, {
-      'title': 'Add Goal',
-      'template': '_goalsForm',
-      data }, document.body);
-  },
-  'click .glyphicon-pencil': function(event) {
-    let goalId = event.target.parentNode.parentNode.id;
-    let data = {
-      'milestone': this.milestone,
-      'goalId': goalId,
-    };
-    Blaze.renderWithData(Template._formModal, {
-      'title': 'Edit Goal',
-      'template': '_editGoal',
-      data }, document.body);
-  },
   'click .bar': function() {
     this.activeType.set(this.milestone.type);
-  },
+  }
 });
