5a5e6caf1e4e1f19604cd37d1b2b85cc46919ecd
Epaminond <k4merton@gmail.com>
2016-01-03 12:57:50 +0200

    Refactor and fix seeds

--- server/startup/loadSeeds.js
+++ server/startup/loadSeeds.js
@@ -3,47 +3,24 @@ and app is in development mode.*/
 
 let mode = process.env.NODE_ENV;
 
-function loadSeeds(user) {
-  let userAlreadyExists = typeof Meteor.users.findOne({
-    'username': user.username }) === 'object';
+Meteor.startup(function() {
+  if (mode != 'development') return;
+  YAML.eval(Assets.getText('users.yml')).users.forEach(user => {
+    if (Meteor.users.find({'username': user.username}).count() > 0) return;
 
-  if (!userAlreadyExists) {
-    let userId = Accounts.createUser(user);
-    let milestones = YAML.eval(Assets.getText('milestones.yml'));
-    let milestoneId = '';
+    let milestones = YAML.eval(Assets.getText('milestones.yml')).milestones;
     let goals = YAML.eval(Assets.getText('goals.yml')).goals;
 
-    for (key in milestones) {
-      if (milestones.hasOwnProperty(key)) {
-        let milestone = milestones[key];
-        milestone.userId = userId;
-        if (milestoneId) {
-          milestone.parentId = milestoneId;
-        }
-        milestoneId = Milestones.insert(milestone);
+    let userId = Accounts.createUser(user);
+    let milestoneId = null;
 
-        if (Milestones.find({}).count() >= 1) {
-          for (key in goals) {
-            if (goals.hasOwnProperty(key)) {
-              let goal = goals[key];
-              goal.userId = userId;
-              goal.milestoneId = milestoneId;
-              Goals.insert(goal);
-            }
-          }
-        }
-      }
-    }
-  }
-}
+    milestones.forEach(milestone => {
+      milestoneData = _.extend(milestone, {userId, parentId: milestoneId});
+      milestoneId = Milestones.insert(milestoneData);
 
-if ( Meteor.isServer && mode === 'development' ) {
-  Meteor.startup(function() {
-    let users = YAML.eval(Assets.getText('users.yml'));
-    for (key in users) {
-      if (users.hasOwnProperty(key)) {
-        loadSeeds(users[key]);
-      }
-    }
+      goals.forEach(goal => {
+        Goals.insert(_.extend(goal, {userId, milestoneId}));
+      });
+    })
   });
-}
+});
