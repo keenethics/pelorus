ceb22ccc751ced814fe688501821c372f22828a5
yataras <taras.yaskiv@gmail.com>
2016-01-04 13:10:11 +0200

    add parent-child relations between goals

--- server/startup/loadSeeds.js
+++ server/startup/loadSeeds.js
@@ -1,10 +1,10 @@
-/*prepopulates db with user's testing data if it's empty
-and app is in development mode.*/
+/* prepopulates db with user's testing data if it's empty
+and app is in development mode. */
 
 let mode = process.env.NODE_ENV;
 
 Meteor.startup(function() {
-  if (mode != 'development') return;
+  if (mode !== 'development') return;
   YAML.eval(Assets.getText('users.yml')).users.forEach(user => {
     if (Meteor.users.find({'username': user.username}).count() > 0) return;
 
@@ -13,14 +13,16 @@ Meteor.startup(function() {
 
     let userId = Accounts.createUser(user);
     let milestoneId = null;
+    let goalId = null;
 
     milestones.forEach(milestone => {
-      milestoneData = _.extend(milestone, {userId, parentId: milestoneId});
+      milestoneData = _.extend(milestone, {userId, 'parentId': milestoneId});
       milestoneId = Milestones.insert(milestoneData);
 
       goals.forEach(goal => {
-        Goals.insert(_.extend(goal, {userId, milestoneId}));
+        goalData = _.extend(goal, {userId, milestoneId, 'parentId': goalId});
+        goalId = Goals.insert(goalData);
       });
-    })
+    });
   });
 });
