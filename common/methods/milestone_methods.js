4edeff5e39d9ae58fc126f287c83b41216ec7d55
OksentukVlad <oksentukvlad@gmail.com>
2016-01-05 14:50:54 +0200

    Copy goals methods.

--- common/methods/milestone_methods.js
+++ common/methods/milestone_methods.js
@@ -25,5 +25,57 @@ Meteor.methods({
   copyMilestoneGoals: function(fromId, toId) {
     check(fromId, String);
     check(toId, String);
+
+    this.unblock();
+
+    if (!this.userId) {
+      throw new Meteor.Error(
+        'forbidden-action',
+        'Can\'t copy goals of parent milectone. User must be logged in.'
+      );
+    }
+
+    const parentMilestone = Milestones.findOne({
+      _id: fromId,
+      userId: this.userId
+    });
+    const childMilestone = Milestones.findOne({
+      _id: toId,
+      userId: this.userId
+    });
+
+    if (!(parentMilestone && childMilestone)) {
+      throw new Meteor.Error(
+        'forbidden-action',
+        `Can\'t copy goals of parent milectone.
+        One or both of milestones are not found.`
+      );
+    }
+
+    const parentGoals = Goals.find({
+      milestoneId: fromId,
+      userId: this.userId,
+      $or: [
+        { completed: false },
+        {
+          completed: {
+            $exists: false
+          }
+        }
+      ]
+    });
+
+    return parentGoals.map(parentGoal => {
+      let childGoal = {
+        title: parentGoal.title,
+        parentId: parentGoal._id,
+        milestoneId: toId,
+        userId: parentGoal.userId,
+        pctOfParentGoal: parentGoal.completedPct,
+        completedPct: 0
+      };
+
+      return Goals.insert(childGoal);
+    });
   }
 });
