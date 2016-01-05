46a267b945bbbc2f0f4eb6017de29607c72045e1
yataras <taras.yaskiv@gmail.com>
2016-01-05 18:42:16 +0200

    add possibility for user to select language

--- client/views/home/_navigation.js
+++ client/views/home/_navigation.js
@@ -4,7 +4,21 @@ Template.navigation.events({
     Blaze.renderWithData(Template._formModal, {
       'title': 'Add Milestone',
       'template': '_milestonesForm', data }, document.body);
+  },
+  'click .user-language': function( e ) {
+    let chosenLang = $(e.target).text();
+    Session.set('language', chosenLang);
+    if (Meteor.user()) {
+      Meteor.users.update({'_id': Meteor.userId()}, {'$set':
+                        {'profile.language': chosenLang}});
+    }
   }
+});
+
+Accounts.onLogin(function() {
+  let language = Session.get('language');
+  Meteor.users.update({'_id': Meteor.userId()}, {'$set':
+                        {'profile.language': language}});
 });
 
 Template.navigation.helpers({
@@ -12,3 +26,11 @@ Template.navigation.helpers({
     return Meteor.userId() && Session.get('canAddMilestones');
   }
 });
+
+if (Meteor.isServer) {
+  Meteor.users.allow({
+    'update': function(userId, user) {
+      return userId === user._id;
+    }
+  });
+}
