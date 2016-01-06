a7c224dd0dd71c8e323c77f0740aba0b6a8332df
yataras <taras.yaskiv@gmail.com>
2016-01-06 13:05:43 +0200

    add reason field to error message

--- common/methods/user_methods.js
+++ common/methods/user_methods.js
@@ -1,7 +1,9 @@
 Meteor.methods({
   'updateUserLanguage': function(chosenLanguage) {
     check(chosenLanguage, String);
-    if (!this.userId) throw new Meteor.Error('User isn\'t logged in.');
+    if (!this.userId) {
+      throw new Meteor.Error('forbidden-action', 'User isn\'t logged in.');
+    }
     return Meteor.users.update(this.userId, {'$set':
                         {'profile.language': chosenLanguage}});
   }
