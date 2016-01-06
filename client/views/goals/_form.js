2ecbb1d9ace2d730fdbbc2baf802d57d1e984d72
Nazar Yablonskiy <nazaryablonskiy@gmail.com>
2016-01-06 10:45:29 +0200

    deleted some code related to milestone/goal creation

--- client/views/goals/_form.js
+++ client/views/goals/_form.js
@@ -1,11 +1,11 @@
 Template._goalsForm.onCreated(function() {
   this.priority = new ReactiveVar(this.data.goal && this.data.goal.priority);
-  this.parent = new ReactiveVar();
+  this.parentId = new ReactiveVar();
 });
 
 Template._goalsForm.onRendered(function() {
-  this.autorun(()=>{
-    let parentId = this.parent.get();
+  this.autorun(() => {
+    let parentId = this.parentId.get();
     let priority = 0;
     let anotherGoal;
     if (parentId) {
@@ -39,7 +39,7 @@ Template._goalsForm.events({
     // TODO: Refactor validations and posting
     let $title = t.$('#title');
     let title = $title.val();
-    let parentId = t.parent.get();
+    let parentId = t.parentId.get();
 
     if (!title) return $title.parent('.form-group').addClass('has-error');
 
@@ -60,7 +60,7 @@ Template._goalsForm.events({
     $('#formModal').modal('hide');
   },
   'change #parentId': function(e, t) {
-    t.parent.set(e.currentTarget.value);
+    t.parentId.set(e.currentTarget.value);
   },
   'change #priority': function(e, t) {
     t.priority.set(Number(e.currentTarget.value));
--- client/views/milestones/_form.js
+++ client/views/milestones/_form.js
@@ -1,6 +1,5 @@
 Template._milestonesForm.onCreated(function() {
   this.selectedType = new ReactiveVar(this.data.type || 'week');
-  this.selectedParentId = new ReactiveVar();
 });
 
 Template._milestonesForm.helpers({
@@ -11,8 +10,6 @@ Template._milestonesForm.helpers({
         'type': Milestones.relativeType(type, -1),
         'userId': Meteor.userId()
       };
-      let firstParent = Milestones.findOne(query);
-      firstParent && Template.instance().selectedParentId.set(firstParent._id);
       return Milestones.find(query);
     }
   },
@@ -29,16 +26,13 @@ Template._milestonesForm.helpers({
   },
   'types': function() {
     return Milestones.validTypes;
-  },
-  'selectedParentId': function() {
-    return Template.instance().selectedParentId.get();
   }
 });
 
 Template._milestonesForm.events({
   'click .js-insert-milestone': function(e, t) {
     let type = t.selectedType.get();
-    let parentId = t.selectedParentId.get();
+    let parentId = t.$('#parentId').val();
 
     // TODO: Refactor validations and posting
     if (type !== 'strategic' && !parentId) {
@@ -59,19 +53,13 @@ Template._milestonesForm.events({
         periodErrMsg.text(` (${err.reason})`);
         return $('#period').parent('.form-group').addClass('has-error');
       }
-      $('#formModal').$modal.modal('hide');
+      $('#formModal').modal('hide');
     });
   },
   'change #type': function(e, t) {
     let type = e.currentTarget.value;
     if (type) {
       t.selectedType.set(type);
-    }
-  },
-  'change #parentId': function(e, t) {
-    let parentId = e.currentTarget.value;
-    if (parentId) {
-      t.selectedParentId.set(parentId);
     }
   }
 });
