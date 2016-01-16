f6032c5cbd840d36875bd7cf9f53f5616fab6735
Epaminond <k4merton@gmail.com>
2016-01-17 00:11:35 +0200

    Minor addMilestone refactoring

--- common/methods/milestone_methods.js
+++ common/methods/milestone_methods.js
@@ -2,20 +2,18 @@ Meteor.methods({
   addMilestone: function(params, copyGoals) {
     check(params, {type: String, period: String});
     check(copyGoals, Match.Optional(Boolean));
-    if (!this.userId) {
+
+    if (!this.userId)
       throw new Meteor.Error('forbidden-action', 'User should be logged in.');
-    }
 
     const locale = Meteor.user().profile.language;
     const bounds = Milestones.boundsFor(params.period, params.type, locale);
     const data = _.extend(params, bounds, {userId: this.userId});
 
-    if (params.type !== 'years' && !Milestones._transform(data).parent()) {
+    if (params.type !== 'years' && !Milestones._transform(data).parent())
       throw new Meteor.Error('period-invalid', 'Parent milestone needed');
-    }
-    if (Milestones._transform(data).siblings().count() > 0) {
+    if (Milestones._transform(data).siblings().count() > 0)
       throw new Meteor.Error('period-invalid', 'Period intersection detected');
-    }
 
     const milestoneId = Milestones.insert(data);
 
