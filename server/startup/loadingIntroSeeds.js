0b3f0082ff338c9b642e1c19af33974ca69694ab
yataras <taras.yaskiv@gmail.com>
2016-01-18 11:13:04 +0200

    add parentId for timeline goals

--- server/startup/loadingIntroSeeds.js
+++ server/startup/loadingIntroSeeds.js
@@ -1,9 +1,10 @@
 
-function loadingSeeds(milestones, goals) {
+function loadingSeeds(milestones, goals, parentGoal) {
   let mchildrens = milestones[0].children;
   let gchildrens = goals[0].children;
   let userId = null;
   let milestoneId = null;
+  let gparent = null;
 
   milestones.forEach(milestone => {
     const bounds = Milestones.boundsFor(milestone.period, milestone.type);
@@ -11,11 +12,11 @@ function loadingSeeds(milestones, goals) {
     milestoneId = Milestones.insert(milestoneData);
 
     goals.forEach(goal => {
-      goalData = _.extend(goal, {userId, milestoneId});
-      Goals.insert(goalData);
+      goalData = _.extend(goal, {userId, milestoneId, parentId: parentGoal});
+      gparent = Goals.insert(goalData);
     });
 
-    if (mchildrens) loadingSeeds(mchildrens, gchildrens);
+    if (mchildrens) loadingSeeds(mchildrens, gchildrens, gparent);
   });
 }
 
