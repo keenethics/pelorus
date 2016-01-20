cd35e457310f12f0ae69408da6fe8abe9c88bd81
abaoaqus <morozova.yara@gmail.com>
2016-01-20 16:52:16 +0200

    Refactored

--- common/methods/user_methods.js
+++ common/methods/user_methods.js
@@ -4,6 +4,24 @@ Meteor.methods({
     if (!this.userId) {
       throw new Meteor.Error('forbidden-action', 'User isn\'t logged in.');
     }
+
+    Milestones.find({userId: this.userId}, {
+      fields: {
+        userId: 1,
+        period: 1,
+        type: 1
+      }
+    }).forEach(function(milestone) {
+      let bounds = Milestones.boundsFor(milestone.period,
+        milestone.type, chosenLanguage);
+      Milestones.update({
+        _id: milestone._id,
+        userId: milestone.userId
+      }, {
+        $set: bounds
+      });
+    });
+
     return Meteor.users.update(this.userId, {
       '$set': {'profile.language': chosenLanguage}});
   }
