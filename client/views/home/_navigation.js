ed116dd1d029db166f62528399632e104717a5d6
yataras <taras.yaskiv@gmail.com>
2016-01-21 11:54:34 +0200

    fix conflicts after merge

---
+++ client/views/home/_navigation.js
@@ -0,0 +1,13 @@
+Template.navigation.events({
+  'click .js-add-milestone': function() {
+    let data = { };
+    Blaze.renderWithData(Template._formModal, {
+      title: 'Add Milestone',
+      template: '_milestonesForm', data }, document.body);
+  },
+  'click .js-set-language': function(e) {
+    let language = $(e.target).data('lang');
+    Session.set('language', language);
+    if (Meteor.userId()) Meteor.call('updateUserLanguage', language);
+  }
+});
