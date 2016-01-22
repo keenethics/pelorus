c7e583ab04d3142e78004e636c2f30f37f46c87d
abaoaqus <morozova.yara@gmail.com>
2016-01-22 18:43:19 +0200

    Refactored Intro steps

--- client/views/home/_navigation.js
+++ client/views/home/_navigation.js
@@ -9,32 +9,7 @@ Template.navigation.events({
   },
   'click #js-run-tutorial': function(e) {
     e.preventDefault();
-    let tutorial = introJs().setOptions({
-      showProgress: true,
-      steps: [
-        {
-          element: '.js-add-stage',
-          intro: Meteor.settings.public.intro.steps.step1,
-          step: 1,
-          position: 'bottom'
-        }, {
-          element: '.js-add-goal',
-          intro: Meteor.settings.public.intro.steps.step2,
-          step: 2,
-          position: 'left'
-        }, {
-          element: '.stage.active .substages .substages .bar',
-          intro: Meteor.settings.public.intro.steps.step3,
-          step: 3,
-          position: 'left'
-        }, {
-          element: '.list-group-item.goal',
-          intro: Meteor.settings.public.intro.steps.step4,
-          step: 4,
-          position: 'bottom'
-        }
-      ]
-    });
+    let tutorial = introJs().setOptions({ showProgress: true });
     Router.go('home', {}, { query: { activeType: 'years' } });
     tutorial.start();
   }
