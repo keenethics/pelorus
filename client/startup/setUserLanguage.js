f2488973f82cc51fffbcc850e2853270902ffc07
yataras <taras.yaskiv@gmail.com>
2016-01-22 17:49:52 +0200

    find out locale from ip address

--- client/startup/setUserLanguage.js
+++ client/startup/setUserLanguage.js
@@ -1,11 +1,16 @@
 Meteor.startup(function() {
   Tracker.autorun(function() {
-    if ( !((Meteor.user() || {}).profile || {}).language ) return;
+    if ( !((Meteor.user() || {}).profile || {}).language ) {
+      return $.getJSON('http://ipinfo.io', function(ipData) {
+        Session.set('language', ipData.country.toLowerCase());
+      });
+    }
     Session.set('language', Meteor.user().profile.language);
   });
 
   Tracker.autorun(function() {
-    const language = Session.get('language') || 'en';
+    let language = Session.get('language') || 'en';
+    if (language === 'ua') language = 'uk';
     TAPi18n.setLanguage(language);
     moment.locale(language);
     accountsUIBootstrap3.setLanguage(language === 'uk' ? 'ua' : language);
