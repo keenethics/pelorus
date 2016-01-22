d0fd61fc24de507d0e4cba98970ea20cc22250fb
abaoaqus <morozova.yara@gmail.com>
2016-01-22 16:16:43 +0200

    Added tutorial using introjs

--- client/views/home/_navigation.js
+++ client/views/home/_navigation.js
@@ -9,5 +9,36 @@ Template.navigation.events({
     let language = $(e.target).data('lang');
     Session.set('language', language);
     if (Meteor.userId()) Meteor.call('updateUserLanguage', language);
+  },
+  'click #js-run-tutorial': function(e) {
+    e.preventDefault();
+    let tutorial = introJs().setOptions({
+      showProgress: true,
+      steps: [
+        {
+          element: '.js-add-milestone',
+          intro: Meteor.settings.public.intro.steps.step1,
+          step: 1,
+          position: 'bottom'
+        }, {
+          element: '.js-add-goal',
+          intro: Meteor.settings.public.intro.steps.step2,
+          step: 2,
+          position: 'left'
+        }, {
+          element: '.milestone.active .submilestones .milestone .milestone',
+          intro: Meteor.settings.public.intro.steps.step3,
+          step: 3,
+          position: 'left'
+        }, {
+          element: '#goal-control-buttons',
+          intro: Meteor.settings.public.intro.steps.step4,
+          step: 4,
+          position: 'left'
+        }
+      ]
+    });
+    Router.go('home', {}, { query: { activeType: 'years' } });
+    tutorial.start();
   }
 });
