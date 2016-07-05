8827374512eab3e75c73c345425ebd3e3d969224
Mykhaylo Fedorovych <misha.fedorovych@gmail.com>
2016-07-05 16:47:01 +0300

    App

--- imports/ui/components/Stage.jsx
+++ imports/ui/components/Stage.jsx
@@ -8,12 +8,34 @@ export default class StageUI extends React.Component {
 	render() {
 		return (
 			<div>
-				{ this.props.stage ?
-					<p>{ this.props.stage.period }</p>
+				{ this.props.active ?
+					<div className={`panel panel-default stage-content`}>
+
+			      		<div className="panel-heading text-capitalize">
+			        		{ this.props.stage.period }
+			        		{ this.props.stage.progress? this.props.stage.progress: '' }
+			      		</div>
+
+								<ul className='list-goal'>
+			      			<li>
+			      				Goal
+			      			</li>
+			      		</ul>
+			      		<div className="panel-body">
+			        		<a href="#" className="btn btn-success pull-right js-add-goal">
+			          		<span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+			        		</a>
+			  			</div>
+			      	</div>
 				:
-					<p>empty</p>
-				}
-			</div>
-		)
+					<div className='panel panel-default bar'>
+	    				<p className='text-capitalize text-warning vertical-text'>
+	      					{ this.props.stage.period }
+	      					{ this.props.stage.progress? this.props.stage.progress: '' }
+					  	</p>
+	  				</div>
+  				}
+	  		</div>
+		);
 	}
 }
