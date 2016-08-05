ae3ebd32b7324ae371ec46ffeefddf555507e6b7
kuksik <nazarkukarkin@gmail.co>
2016-08-05 16:24:17 +0300

    test removeGoal method #1

--- imports/api/goals/methods/remove.js
+++ imports/api/goals/methods/remove.js
@@ -6,8 +6,6 @@ import { Goals } from '/imports/api/goals/goals.js'
 export const removeGoal = new ValidatedMethod({
   name: 'goal.remove',
   validate({ goalData }) {
-    console.log('valuidate')
-    console.log(goalData)
     check(goalData, {
       _id: String,
       title: String,
@@ -19,7 +17,7 @@ export const removeGoal = new ValidatedMethod({
     });
   },
   run({ goalData }) {
-    if (this.userId !== goalData.userId || Goals.findOne({_id: goalData._id}).children().count()) {
+    if (this.userId !== goalData.userId || Goals.findOne({_id: goalData._id}).children().fetch().length) {
       throw new Meteor.Error('forbidden-action', 'Goal can\'t be removed');
     }
     return Goals.remove({ _id: goalData._id });
--- test/server/goals.test.js
+++ test/server/goals.test.js
@@ -1,11 +1,29 @@
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { Goals } from '/imports/api/goals/goals.js';
-import { insertGoal } from '/imports/api/goals/methods/insert.js';
 import faker from 'faker';
 import { Accounts } from 'meteor/accounts-base';
 import { Random } from 'meteor/random';
+import { Factory } from 'meteor/dburles:factory';
+import { insertGoal } from '/imports/api/goals/methods/insert.js';
+import { removeGoal } from '/imports/api/goals/methods/remove.js';
 
 const langs = ["uk", "ru", "en" ];
+const userId = Accounts.createUser({
+  username: 'username',
+  email : 'useremail@mail.com',
+  password : "password",
+  profile  : {
+    language: langs[Math.round(Random.fraction()*2 )],
+  },
+});
+
+Factory.define('goals', Goals, {
+  title: faker.name.title(),
+  rank: Math.round(Random.fraction()*10),
+  progress: Math.round(Random.fraction()*100),
+  stageId: Random.id(),
+  parentId: Random.id(),
+});
 
 describe('goals', function() {
   beforeEach(function () {
@@ -13,14 +31,6 @@ describe('goals', function() {
   });
   describe('addGoal', function() {
     it('should add new goal', function() {
-      const userId = Accounts.createUser({
-        username: 'username',
-        email : 'useremail@mail.com',
-        password : "password",
-        profile  : {
-          language: langs[Math.round(Random.fraction()*2 )],
-        },
-      });
       const goalData = {
         title: faker.name.title(),
         rank: Math.round(Random.fraction()*10),
@@ -41,5 +51,25 @@ describe('goals', function() {
       assert.equal(goalData.parentId, newGoal.parentId);
       assert.equal(userId, newGoal.userId);
     });
+  });
+  describe('removeGoal', function() {
+      it('should remove goal without children', function() {
+
+        Factory.create('goals', {
+          title: faker.name.title(),
+          rank: Math.round(Random.fraction()*10),
+          progress: Math.round(Random.fraction()*100),
+          stageId: Random.id(),
+          parentId: Random.id(),
+          userId: userId,
+        });
+        const goalData = Goals.findOne();
+        removeGoal.run.call(
+          { userId: userId },
+          { goalData }
+        );
+
+        assert.equal(Goals.find().count(), 0);
+      });
   });
 })
